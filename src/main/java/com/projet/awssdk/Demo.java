1e022cd4eb4cee3cdd6f6821e19c9ddc4191149e
Mathieu Bolla <mathieu.bolla@gmail.com>
2013-11-23 12:12:08 +0100

    Etape 14: Les zombies jouent à Maven!

--- src/main/java/com/projet/awssdk/Demo.java
+++ src/main/java/com/projet/awssdk/Demo.java
@@ -1,9 +1,14 @@
 package com.projet.awssdk;
 
-import java.io.ByteArrayInputStream;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Date;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.apache.commons.lang3.StringUtils.join;
 
 public class Demo {
     public static final String BUCKET = "code.projet.com";
@@ -11,11 +16,13 @@ public class Demo {
     private static final String JAR_NAME = "code.jar";
     private static final String JAR_TYPE = "application/java-archive";
 
-    public static final int MACHINE_COUNT = 1;
+    public static final int MACHINE_COUNT = 5;
     public static final String CLASS_NAME = Demo.class.getCanonicalName();
 
     public static final String COMMANDS_QUEUE = "commands";
     public static final String CONQUESTS_QUEUE = "conquests";
+
+    private static final int MAX_COUNT = 1000;
 
     public static void main(String... args) throws IOException {
         ClientsManager clientsManager = new ClientsManager();
@@ -33,8 +40,14 @@ public class Demo {
     }
 
     private static void command(EC2Manager ec2Manager, S3Manager s3Manager,
-                                IAMManager iamManager, SQSManager sqsManager) throws IOException {
+                                IAMManager iamManager,
+                                final SQSManager sqsManager) throws IOException {
 
+        prepare(ec2Manager, s3Manager, iamManager, sqsManager);
+        work(sqsManager);
+    }
+
+    private static void prepare(EC2Manager ec2Manager, S3Manager s3Manager, IAMManager iamManager, SQSManager sqsManager) throws IOException {
         String commandsQueueArn = sqsManager.create(COMMANDS_QUEUE);
         String conquestQueueArn = sqsManager.create(CONQUESTS_QUEUE);
 
@@ -59,28 +72,75 @@ public class Demo {
 
         ec2Manager.run(
                 script, MACHINE_COUNT, instanceProfileName);
+    }
 
-        sqsManager.sendMessage("Hey, zombies!", COMMANDS_QUEUE);
-        if (!sqsManager.processInTransaction(new SQSManager.TransactionalProcess() {
+    private static void work(final SQSManager sqsManager) {
+        final Set<String> urlDone = new HashSet<String>();
+
+        sqsManager.sendMessage("http://www.goandcloud.it", COMMANDS_QUEUE);
+
+        while (sqsManager.processInTransaction(new SQSManager.TransactionalProcess() {
             @Override
             public void process(String input) {
-                System.out.println("All right, lets stop here");
+                for (String urlFound : StringUtils.split(input, '\n')) {
+                    if (!urlDone.contains(urlFound) && urlDone.size() < MAX_COUNT) {
+                        System.out.println("Go, process " + urlFound);
+                        sqsManager.sendMessage(urlFound, COMMANDS_QUEUE);
+                        urlDone.add(urlFound);
+                    }
+                }
             }}, CONQUESTS_QUEUE)) {
 
-            System.out.println("Defeat!");
+            System.out.println("Found new lands! Lets go on!");
         }
     }
 
     private static void conquer(final S3Manager s3Manager,
                                 final SQSManager sqsManager) {
-        sqsManager.processInTransaction(new SQSManager.TransactionalProcess() {
+        while (proceed(s3Manager, sqsManager)) {
+            System.out.println("What next, commander?");
+        }
+        System.out.println("Doh, we won war? Really?");
+    }
+
+    private static boolean proceed(final S3Manager s3Manager, final SQSManager sqsManager) {
+        return sqsManager.processInTransaction(new SQSManager.TransactionalProcess() {
             @Override
             public void process(String input) {
+                final Set<String> urlsFoundInContent = new HashSet<String>();
+
+                InputStream urlStream = UrlUtils.download(
+                        input,
+                        new UrlUtils.FoundCallback<String>() {
+                            @Override
+                            public void found(String element) {
+                                urlsFoundInContent.add(element);
+                            }
+                        });
+
                 s3Manager.upload(
-                        new ByteArrayInputStream(input.getBytes()),
-                        BUCKET, "results/" + new Date().toString(), JAR_TYPE);
-                sqsManager.sendMessage("Roger!", CONQUESTS_QUEUE);
+                        urlStream,
+                        BUCKET,
+                        "results/" + cleanupUrlForS3(input),
+                        "application/octet-stream");
+
+                try {
+                    urlStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                String content = join(urlsFoundInContent, '\n');
+
+                if (!content.isEmpty()) {
+                    sqsManager.sendMessage(content, CONQUESTS_QUEUE);
+                }
+
             }
         }, COMMANDS_QUEUE);
+    }
+
+    private static String cleanupUrlForS3(String url) {
+        return url.replace("http://", "").replace("https://", "");
     }
 }
